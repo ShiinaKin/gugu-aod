e0a9ef13d8e561cb77cb0b537918e7f74d50b276
mashirot <shiina@sakurasou.io>
2024-01-06 18:09:23 +0800

    feat: 增加对VLC环境检测的提示

--- ui/src/main/kotlin/ski/mashiro/view/HomeView.kt
+++ ui/src/main/kotlin/ski/mashiro/view/HomeView.kt
@@ -1,16 +1,21 @@
 package ski.mashiro.view
 
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Divider
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import ski.mashiro.component.AudioPlayerComponent
+import ski.mashiro.common.GlobalBean
+import ski.mashiro.component.MediaPlayerComponent
 import ski.mashiro.component.PlayListComponent
+import ski.mashiro.component.player.GuGuMediaPlayerController
+import uk.co.caprica.vlcj.factory.discovery.NativeDiscovery
 
 /**
  * @author mashirot
@@ -27,6 +32,22 @@ fun HomeView() {
             PlayListComponent()
         }
         Divider(modifier = Modifier.fillMaxWidth().padding(0.dp, 4.dp))
-        AudioPlayerComponent()
+        if (NativeDiscovery().discover()) {
+            MediaPlayerComponent()
+        } else {
+            Box(
+                modifier = Modifier.fillMaxSize().align(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = "缺少VLC环境, 请安装VLC",
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+        LaunchedEffect(GlobalBean.musicList.size) {
+            if (GlobalBean.musicList.isNotEmpty() && !GuGuMediaPlayerController.hasMusic()) {
+                GuGuMediaPlayerController.autoPlayNext()
+            }
+        }
     }
 }
