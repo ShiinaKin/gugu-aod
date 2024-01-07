c23a79b80a3d41e6a121e8c8b5b6ee9429e6d451
mashirot <shiina@sakurasou.io>
2024-01-08 02:43:19 +0800

    feat: RoomConfig构造修改

--- model/src/main/kotlin/ski/mashiro/entity/config/RoomConfig.kt
+++ model/src/main/kotlin/ski/mashiro/entity/config/RoomConfig.kt
@@ -19,13 +19,14 @@ data class RoomConfig(
     constructor(
         roomId: Long,
         uid: Long,
+        cookie: String
     ) : this(
         roomId,
         null,
         uid,
         "850221AA-6657-6657-6657-91043281097552522infoc",
         "",
-        "",
+        cookie,
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     )
 }
