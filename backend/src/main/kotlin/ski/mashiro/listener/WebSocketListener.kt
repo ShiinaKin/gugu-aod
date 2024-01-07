30cf9f494911c041e1ed3fbdb052d478d72c5844
mashirot <shiina@sakurasou.io>
2024-01-08 02:36:12 +0800

    feat: WebSocketListener增加日志

--- backend/src/main/kotlin/ski/mashiro/listener/WebSocketListener.kt
+++ backend/src/main/kotlin/ski/mashiro/listener/WebSocketListener.kt
@@ -5,6 +5,8 @@ import okhttp3.Response
 import okhttp3.WebSocket
 import okhttp3.WebSocketListener
 import okio.ByteString
+import ski.mashiro.annotation.Logger
+import ski.mashiro.annotation.Logger.Companion.log
 import ski.mashiro.common.GlobalBean
 import ski.mashiro.handler.MessageHandler
 import ski.mashiro.service.impl.WebSocketServiceImpl
@@ -14,17 +16,17 @@ import ski.mashiro.util.HeartbeatUtils
 /**
  * @author mashirot
  */
+@Logger
 class WebSocketListener : WebSocketListener() {
 
     override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
-        println("closed")
+        log.debug { "ws closed" }
         HeartbeatTimer.stop()
     }
 
     override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
-        println("=====ERR=====")
-        println(t.stackTraceToString())
-        println("=============")
+        log.warn { "ws error" }
+        log.warn { t }
         HeartbeatTimer.stop()
         webSocket.cancel()
         GlobalBean.webSocket = null
@@ -38,7 +40,7 @@ class WebSocketListener : WebSocketListener() {
     }
 
     override fun onOpen(webSocket: WebSocket, response: Response) {
-        println("Opening")
+        log.debug { "ws opined" }
         webSocket.send(HeartbeatUtils.generateAuthorizeBag())
         HeartbeatTimer.start(webSocket)
     }
