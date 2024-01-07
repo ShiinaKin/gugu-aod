8fb6053e10c7754700d4a9853348932164e211d3
mashirot <shiina@sakurasou.io>
2024-01-08 02:33:39 +0800

    feat: 心跳包Timer增加日志

--- backend/src/main/kotlin/ski/mashiro/timer/HeartbeatTimer.kt
+++ backend/src/main/kotlin/ski/mashiro/timer/HeartbeatTimer.kt
@@ -2,15 +2,18 @@ package ski.mashiro.timer
 
 import kotlinx.coroutines.*
 import okhttp3.WebSocket
+import ski.mashiro.annotation.Logger
+import ski.mashiro.annotation.Logger.Companion.log
 import ski.mashiro.util.HeartbeatUtils
 import kotlin.time.Duration
 
 /**
  * @author mashirot
  */
+@Logger
 object HeartbeatTimer {
 
-    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
 
     fun start(webSocket: WebSocket) {
         coroutineScope.launch {
@@ -19,18 +22,16 @@ object HeartbeatTimer {
     }
 
     fun stop() {
-        println("stop")
         coroutineScope.cancel()
+        log.debug { "HeartbeatTimer stopped" }
     }
 
     private suspend fun sendHeartbeatBag(webSocket: WebSocket) {
-        withContext(Dispatchers.IO) {
-            delay(Duration.parse("2s"))
-            while (true) {
-                webSocket.send(HeartbeatUtils.generateHeartbeatBag())
-                println("Ping!!!")
-                delay(Duration.parse("30s"))
-            }
+        delay(Duration.parse("2s"))
+        while (true) {
+            webSocket.send(HeartbeatUtils.generateHeartbeatBag())
+            log.debug { "Ping!!!" }
+            delay(Duration.parse("30s"))
         }
     }
 
