a8ddb836d0c5847019ce934e0f825f95792c7f58
mashirot <shiina@sakurasou.io>
2024-01-08 23:20:21 +0800

    feat: 移除最小窗口大小限制，修改初始窗口大小
    swing的单位和compose的单位没法转换，索性不要了

--- ui/src/main/kotlin/ski/mashiro/Main.kt
+++ ui/src/main/kotlin/ski/mashiro/Main.kt
@@ -12,27 +12,26 @@ import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import moe.tlaster.precompose.PreComposeApp
 import ski.mashiro.page.HomePage
-import java.awt.Dimension
 
 /**
  * @author mashirot
  * 2024/1/3 15:03
  */
 fun main() = application {
+    val state = rememberWindowState(
+        width = 500.dp,
+        height = 750.dp,
+        position = WindowPosition(Alignment.Center)
+    )
     Window(
         title = "GuGu-AOD",
         icon = painterResource("icon.ico"),
-        state = rememberWindowState(
-            width = 500.dp,
-            height = 700.dp,
-            position = WindowPosition(Alignment.Center)
-        ),
+        state = state,
         onCloseRequest = {
             BackendMain.onClose()
             exitApplication()
         }
     ) {
-        window.minimumSize = Dimension(600, 400)
         BackendMain.init()
         App()
     }
