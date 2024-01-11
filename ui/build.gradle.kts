b03ddf5df43777af01a95674ede6a63481a21049
mashirot <shiina@sakurasou.io>
2024-01-11 23:47:15 +0800

    同步全局版本号

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -32,7 +32,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Exe)
             packageName = "gugu-aod"
-            packageVersion = "1.0.2"
+            packageVersion = version as String
             licenseFile.set(project.file("../LICENSE"))
 
             windows {
