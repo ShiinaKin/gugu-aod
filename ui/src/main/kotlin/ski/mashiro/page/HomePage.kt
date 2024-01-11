ee771e41f51f56964b8455063d9181bfb12313be
mashirot <shiina@sakurasou.io>
2024-01-11 22:14:13 +0800

    fix: 移除网易云登陆状态监听，避免频繁调用接口导致接口风控

--- ui/src/main/kotlin/ski/mashiro/page/HomePage.kt
+++ ui/src/main/kotlin/ski/mashiro/page/HomePage.kt
@@ -1,21 +1,16 @@
 package ski.mashiro.page
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.AlertDialog
 import androidx.compose.material.Divider
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
 import ski.mashiro.common.GlobalBean
 import ski.mashiro.component.MenuComponent
 import ski.mashiro.component.player.GuGuMediaPlayerController
 import ski.mashiro.router.Router
-import ski.mashiro.service.impl.NeteaseCloudMusicServiceImpl
 
 /**
  * @author mashirot
@@ -23,34 +18,6 @@ import ski.mashiro.service.impl.NeteaseCloudMusicServiceImpl
  */
 @Composable
 fun HomePage() {
-    var tempFailedMsg by remember { mutableStateOf("") }
-    var showFailedDialog by remember { mutableStateOf(false) }
-    if (showFailedDialog) {
-        AlertDialog(
-            modifier = Modifier.width(250.dp).height(120.dp),
-            text = {
-                Text(
-                    text = tempFailedMsg,
-                    textAlign = TextAlign.Start
-                )
-            },
-            onDismissRequest = {
-                showFailedDialog = false
-            },
-            confirmButton = {
-                TextButton(
-                    onClick = {
-                        showFailedDialog = false
-                    },
-                    modifier = Modifier.height(40.dp)
-                ) {
-                    Text(
-                        text = "OK"
-                    )
-                }
-            }
-        )
-    }
     Row(modifier = Modifier.fillMaxSize()) {
         MenuComponent()
         Divider(
@@ -62,14 +29,6 @@ fun HomePage() {
                 .fillMaxSize()
         ) {
             Router.navController = Router.initNavController()
-        }
-    }
-    GlobalBean.IO_SCOPE.launch {
-        runCatching {
-            GlobalBean.neteaseCloudMusicLoginStatus = NeteaseCloudMusicServiceImpl.getLoginStatus()
-        }.getOrElse {
-            tempFailedMsg = it.message!!
-            showFailedDialog = true
         }
     }
     LaunchedEffect(GlobalBean.musicList.size) {
